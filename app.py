40c8b58ad277c034e066109eb68a8290268f6028
unknown <swarika42@gmail.com>
2025-06-29 00:34:19 +0530

    minor changhe

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ if __name__ == "__main__":
 
     print("✅ Bot Thread Started, Launching Web Server...")
 
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
